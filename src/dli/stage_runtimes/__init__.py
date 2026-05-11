2ec06cbceb8f1fd028e9d1e1aad7de5ef54f0c7d
mihai-daian <daian.pe.petre@student.utcluj.ro>
2026-05-11 10:36:07 +0300

    updated folder structure

--- src/dli/stage_runtimes/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Inference stage runtime implementations."""
-
--- src/dli/stage_runtimes/python_legacy/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Inference Stage package.
-
-Each Inference Stage owns one static model partition and executes a subset
-of transformer components during distributed inference.
-"""
